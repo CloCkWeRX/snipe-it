325afc8dc39c177ed836308d35acc1bddc6dae44
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-04-19 13:39:59 +0930

    Add tests

--- tests/Feature/Companies/Ui/EditCompanyTest.php
+++ tests/Feature/Companies/Ui/EditCompanyTest.php
@@ -14,4 +14,22 @@ class EditCompanyTest extends TestCase
             ->get(route('companies.edit', Company::factory()->create()))
             ->assertOk();
     }
+
+    public function testUserCanEditCompanies()
+    {
+        $company = Company::factory()->create(['name' => 'Test Company']);
+        $this->assertTrue(Company::where('name', 'Test Company')->exists());
+        $response = $this->actingAs(User::factory()->superuser()->create())
+            ->put(route('companies.update', $manufacturer), [
+                'name' => 'Test Company Edited',
+                'notes' => 'Test Note Edited',
+                'url' => 'http://example.com',
+                'wikidata' => 'Q12345'
+            ])
+            ->assertStatus(302)
+            ->assertSessionHasNoErrors()
+            ->assertRedirect(route('companies.index'));
+        $this->followRedirects($response)->assertSee('Success');
+        $this->assertTrue(Company::where('name', 'Test Company Edited')->where('notes', 'Test Note Edited')->where('url', 'http://example.com')->where('wikidata', 'Q12345')->exists());
+    }
 }
