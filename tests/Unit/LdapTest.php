fea5e334ce21d265bfb9ecc058120ef5cdedd981
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-04-20 19:53:02 +0930

    Ensure test case controls the environment better, by adding a sleep (#72)
    * PSR12
    
    * Fix flaky test behaviour by ensuring file modification time is always after settings changes

--- tests/Unit/LdapTest.php
+++ tests/Unit/LdapTest.php
@@ -225,8 +225,12 @@ class LdapTest extends TestCase
     public function testFreshTLSFile()
     {
         $this->settings->enableLdap()->set(['ldap_client_tls_cert' => 'SAMPLE CERT TEXT']);
+
         $client_side_cert_path = Setting::get_client_side_cert_path();
+
+        sleep(1); // Ensure that settings vs file write time are distinct. TODO: https://github.com/hnw/php-timecop to do this properly?
         file_put_contents($client_side_cert_path, 'WEIRDLY UPDATED CERT FILE');
+
         //the system should respect our cache-file, since the settings haven't been updated
         $possibly_recached_cert_file = Setting::get_client_side_cert_path(); //this should *NOT* re-cache from the Settings
         $this->assertStringEqualsFile($possibly_recached_cert_file, 'WEIRDLY UPDATED CERT FILE');
