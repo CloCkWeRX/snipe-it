319c7f00860c74a5fbf2adcce7943ee0ed7061f6
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-04-27 05:27:09 +0000

    Cleanup

--- app/Importer/DepreciationImporter.php
+++ app/Importer/DepreciationImporter.php
@@ -23,53 +23,54 @@ class DepreciationImporter extends ItemImporter
     public function createDepreciationIfNotExists(array $row)
     {
 
-        $editingLocation = false;
+        $editingDepreciation = false;
 
         $depreciation = Depreciation::where('name', '=', $this->findCsvMatch($row, 'name'))->first();
 
         if ($this->findCsvMatch($row, 'id') != '') {
-            // Override location if an ID was given
-            \Log::debug('Finding location by ID: ' . $this->findCsvMatch($row, 'id'));
-            $location = Location::find($this->findCsvMatch($row, 'id'));
+            // Override depreciation if an ID was given
+            \Log::debug('Finding depreciation by ID: ' . $this->findCsvMatch($row, 'id'));
+            $depreciation = Depreciation::find($this->findCsvMatch($row, 'id'));
         }
 
 
-        if ($location) {
+        if ($depreciation) {
             if (! $this->updating) {
-                $this->log('A matching Location ' . $this->item['name'] . ' already exists');
+                $this->log('A matching depreciation ' . $this->item['name'] . ' already exists');
                 return;
             }
 
-            $this->log('Updating Location');
-            $editingLocation = true;
+            $this->log('Updating Depreciation');
+            $editingDepreciation = true;
         } else {
-            $this->log('No Matching Location, Create a new one');
-            $location = new Location();
-            $location->created_by = auth()->id();
+            $this->log('No Matching Depreciation, Create a new one');
+            $depreciation = new Depreciation();
+            $depreciation->created_by = auth()->id();
         }
 
         // Pull the records from the CSV to determine their values
         $this->item['name'] = trim($this->findCsvMatch($row, 'name'));
+        $this->item['months'] = trim($this->findCsvMatch($row, 'months'));
 
         Log::debug('Item array is: ');
         Log::debug(print_r($this->item, true));
 
 
-        if ($editingLocation) {
-            Log::debug('Updating existing location');
-            $location->update($this->sanitizeItemForUpdating($location));
+        if ($editingDepreciation) {
+            Log::debug('Updating existing depreciation');
+            $depreciation->update($this->sanitizeItemForUpdating($depreciation));
         } else {
-            Log::debug('Creating location');
-            $location->fill($this->sanitizeItemForStoring($location));
+            Log::debug('Creating depreciation');
+            $depreciation->fill($this->sanitizeItemForStoring($depreciation));
         }
 
-        if ($location->save()) {
-            $this->log('Location ' . $location->name . ' created or updated from CSV import');
-            return $location;
+        if ($depreciation->save()) {
+            $this->log('Depreciation ' . $depreciation->name . ' created or updated from CSV import');
+            return $depreciation;
         } else {
-            Log::debug($location->getErrors());
-            $this->logError($location, 'Location "' . $this->item['name'] . '"');
-            return $location->errors;
+            Log::debug($depreciation->getErrors());
+            $this->logError($depreciation, 'Depreciation "' . $this->item['name'] . '"');
+            return $depreciation->errors;
         }
     }
 }
